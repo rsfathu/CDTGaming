8c10623e01136308f126ee2d17e27bf922823ebd
trollorder <73745120+trollorder@users.noreply.github.com>
2021-11-30 12:07:14 +0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -14,131 +14,97 @@ fee1c7- Beige
 52d1dc- cyan 
 110b11- midnight blue
  """
+class Round:
+    def __init__(self):
+        self.userboolean = False # True is for Player 1, False is for Player 2
+        self.player1index,self.player2index = 0,0 #This indicates player position on the board and is passed into the playing board to display
+        #positions
+        self.player1score,self.player2score = 0,0 #This indicates overall questions answered correctly.
+        self.endgameboolean = False #Will always be false until the end turn check returns true.
+        #Upon being True, the game will show the endgame frame and the next move if any, is to reset the Round.
+        self.questionstring = ""
+        self.mcq1,self.mcq2,self.mcq3,self.mcq4="","","",""
+        self.correctans = ""
+        self.useranscorrect = None #This is for the display on the answer screen.
+        #above are all the variables required to run questionframemcq and to serve to the answer screen
+        self.excludedlistofqns = [] #All elements in this list can not be called again.
 
-"""
-class p1_turn():
-    def __init__(self,index):
-        self.index = index
-    def bot_left(self): #This is the method to find the coordinates of bottom left of player's icon
-        j = self.index % 20
-        if j > 0 and j < 11:
-            x = 60 * (j-1)
+    def reset(self):
+        self.userboolean = False 
+        self.player1index,self.player2index = 0,0 
+        self.player1score,self.player2score = 0,0 
+        self.endgame = False 
+        self.questionstring = ""
+        self.mcq1,self.mcq2,self.mcq3,self.mcq4="","","",""
+        self.correctans = ""
+        self.useranscorrect = None 
+        self.excludedlistofqns = [] 
+    
+    def endgame(self):
+        if self.index1index or self.player2index >= 100:
+            self.endgameboolean = True
         else:
-            x = 540 - 60 * (j - 11)
-        m = x + 30
-        k = int(self.index / 10)
-        y = 600 - 60 * k
-        n = y - 30
-        return x, y, m, n
-    def check_index(self):
-        if self.index in [1,4,7,9,10,32,45,78]:
-            print("snake")
-        elif self.index in [3,12,15,35,49]:
-            print("ladder")
-        else:
-            pass
-    def roll_dice(self):
-        start = input("P1's turn go: ")
-        if start != "0":
-            pass
-        else:
-            print("DIE")
-        die_num = int(random.randint(1, 6))
-        self.index = self.index + die_num
-        print(self.index)
-        return
+            return False 
 
-class p2_turn():
-    def __init__(self, index):
-        self.index = index
-    def bot_left(self):  #This is the method to find the coordinates of bottom left of player's icon
-        j = self.index % 20
-        if j > 0 and j < 11:
-            x = 60 * (j-1)
-        else:
-            x = 540 - 60 * (j - 11)
-        m = x + 30
-        k = int(self.index / 20)
-        y = 600 - 120 * k
-        n = y - 30
-        return x, y, m, n
-    def check_index(self):
-        if self.index in [4, 7, 9, 10, 32, 45, 78]:
-            print("snake")
-        elif self.index in [3, 12, 15, 35, 49]:
-            print("ladder")
-        else:
-            pass
-    def roll_dice(self):
-        start = input("P2's turn go: ")
-        if start != "0":
-            pass
-        else:
-            print("DIE")
-        die_num = int(random.randint(1, 6))
-        self.index = self.index + die_num
-        print(self.index)
-        return
+class GUILOOP():
+    def __init__(self) :
+        self.roundstart = Round() #roundstart is the object for holding all information
+        self.rootmaster = Tk()
+        self.rootmaster.geometry("1280x720")
+        self.rootmaster.configure(background="#52d1dc")
+        #self.nextframe = Playingboard(self.rootmaster,10,1,'ladder',None,None)
+        self.nextframe = Answerframe(self.rootmaster)
+        self.start  = Introframe(self.rootmaster,self.nextframe)
+        self.rootmaster.mainloop()
 
-hi = True
-end = 0
-p1 = p1_turn(1)
-p2 = p2_turn(1)
-while end == 0:
-    if hi == True:
-        p1.roll_dice()
-        print("p1 turn done")
-        print(p1.bot_left())
-        hi = False
-    else:
-        p2.roll_dice()
-        print("p2 turn done")
-        print(p2.bot_left())
-        hi = True
-"""
-
-class Introframe:
-    def __init__(self, master,nextframe):
-
+class SNLFrame(GUILOOP):
+    def __init__(self, master):
         self.master = Frame(master, width=1280 , height=720,background="#52d1dc")
-        
+
+    def changeframe(self,future):
+        future.master.grid(row=0,column=0)
+        self.master.grid_forget()
+        future.master.tkraise()
+
+
+class Introframe(SNLFrame):
+    def __init__(self, master, greet_nextframe):
+        super().__init__(master)
         master.title("Snakes and Ladders Know it All")
 
         self.label = Label(self.master, text="Snakes and Ladders, KNOW IT ALL!!! " , font= ("Corbel" , 42) , foreground="#110b11", background="#fee1c7" ,padx=10,pady=20)
         self.label.place(x=300 , y = 200)
 
-        self.greet_button = Button(self.master, text="Let's Begin, Hajimasho!",font=("Roboto",24), padx=10 , pady=10 , command=lambda : self.changeframe(nextframe))
+        self.greet_nextframe = greet_nextframe
+        self.greet_button = Button(self.master, text="Let's Begin, Hajimasho!",font=("Roboto",24), padx=10 , pady=10 , command=self.greet_button_pressed)
         self.greet_button.place(x=600, y=320)
 
         self.close_button = Button(self.master, text="Quit Game", command=master.quit , font=("Roboto" , 24) , background="#fee1c7" , foreground="#110b11")
         self.close_button.place(x=1000,y=600)
         self.master.grid(row=0,column=0)
-    def changeframe(self,future):
-        future.master.grid(row=0,column=0)
-        self.master.grid_forget()
-        future.master.tkraise()
-    def proceed(self):
-        pass
+    def greet_button_pressed(self):
+        self.changeframe(self.greet_nextframe)
 
-class Endgameframe():
+
+class Endgameframe(SNLFrame):
     def __init__(self, master):
-        self.master = Frame(master, width=1280 , height=720,background="#52d1dc")
+        super().__init__(master)
         master.title("Thanks for playing!")
 
         self.label = Label(self.master, text="Rate us on Playstore 5/5, Hope you enjoyed the game​" , font= ("Corbel" , 36) , foreground="#110b11", background="#fee1c7" ,padx=10,pady=20)
         self.label.place(x=250 , y = 200)
 
-        self.restart_button = Button(self.master, text="Restart?", command=None , height= 4 , width= 40 , padx= 10 , pady = 10 , font=("roboto" , 24) , background="#f991cc" , foreground="#110b11")
+        self.restart_button = Button(self.master, text="Restart?", command=self.restartthegame , height= 4 , width= 40 , padx= 10 , pady = 10 , font=("roboto" , 24) , background="#f991cc" , foreground="#110b11")
         self.restart_button.place(x=400 , y=320)
 
         self.close_button = Button(master, text="Quit Game", command=master.quit , font=("Roboto" , 24) , background="#fee1c7" , foreground="#110b11")
         self.close_button.place(x=1000,y=700)
+    def restartthegame(self): #problem with accessing the object 
+        self.roundstart.reset()
+        self.changeframe(Introframe(self.rootmaster))
 
-    def changeframe(self,future):
-        future.master.grid(row=0,column=0)
-        self.master.grid_forget()
-        future.master.tkraise()
 
+    
 class QuestionframeMCQ :
     def __init__(self,master,mcq1 = "MCQ1 QUESTION ",mcq2="MCQ2 QUESTION ",mcq3="MCQ3 QUESTION ",mcq4="MCQ4 QUESTION ",questionlabel=" Insert something here") -> None:
         self.master = Frame(master, width=1280 , height=720,background="#52d1dc")
@@ -161,12 +127,11 @@ class QuestionframeMCQ :
         self.mcqbutton4.place(x=800,y=500)
         self.nextbutton.place(x=1000,y=650)
 
-    def nextpg(self, pagename):
-        pass
 
+#Class below optional
 class QuestionframeOPEN:
     def __init__(self,master,questionlabel = "Question to be inserted") -> None:
-        self.master = Frame(master, width=1280 , height=720,background="#52d1dc")
+        super().__init__(master)
         master.title("Question Time!")
         self.questionlabel= questionlabel
 
@@ -178,11 +143,12 @@ class QuestionframeOPEN:
         self.qlabel.place(x=200 , y=100)
         self.nextbutton.place(x=1000,y=650)
         self.answerbox.place(x=100,y=300)
+
     def nextpg(self):
         pass
-class Answerframe :
-    def __init__(self,master,answerstring =" You didn't specify a question",userboolean = None) -> None:
-        self.master = Frame(master, width=1280 , height=720,background="#52d1dc")
+class Answerframe(SNLFrame) :
+    def __init__(self,master,answerstring =" You didn't specify a question",userboolean = None, playerturn = None,player1points = 0,player2points = 0) -> None:
+        super().__init__(master)
         self.answer = answerstring
         self.boolean = userboolean #this thing is a boolean that will display different messages for correct or wrong messages.
         if self.boolean == True:
@@ -193,7 +159,7 @@ class Answerframe :
             self.displaytext = "Amazing you managed to find a loophole."
         #declaration
         self.answerlabel = Label(self.master,text= "ANSWER HERE",font= ("Corbel" , 36) , foreground="#110b11", background="#fee1c7" ,padx=10,pady=20)
-        self.nextbutton = Button(self.master, text="Continue", command=self.nextpg() , font=("Roboto" , 24) , background="#fee1c7" , foreground="#110b11")
+        self.nextbutton = Button(self.master, text="Continue", command=self.changeframe(Playingboard(master)), font=("Roboto" , 24) , background="#fee1c7" , foreground="#110b11")
         self.congratulations = Label(self.master,text= self.displaytext,font= ("Corbel" , 36) , foreground="#110b11", background="#fee1c7" ,padx=10,pady=20)
         if self.boolean == True:
             self.useriscorrect = True
@@ -203,13 +169,20 @@ class Answerframe :
         self.answerlabel.place(x=200,y=100)
         self.congratulations.place(x=200,y=300)
         self.nextbutton.place(x=1000,y=650)
+
+    def addpoints(self):
+        if self.userboolean:
+            self.currentpts = self.currentpts + 1
+        elif self.correctanot == False:
+            pass
+        else:
+            return None ### fullproof code in case input is neither True/False
+        return (self.player, self.currentpts) ### use tuple to reduce hacking risks as values is not mutable
     
-    def nextpg(self):
-        #this will key in variables to the next page.
-        return Playingboard()
-class Explanationframe :
+class Explanationframe(GUILOOP) :
     def __init__(self,master) -> None:
         self.master = Frame(master, width=1280 , height=720,background="#52d1dc")
+        super().__init__(master)
         self.explanation= """
         You roll a dice. Then you move that many steps​
         \n
@@ -226,17 +199,18 @@ class Explanationframe :
         """
         self.explanationheader = Label(self.master, text= "Rules As Below...", font= ("Corbel" , 32) , foreground="#110b11", background="#fee1c7" ,padx=10,pady=20)
         self.explanationtext = Label(self.master,text= self.explanation,font= ("Roboto",12) , foreground="#110b11", background="#fee1c7" ,padx=10,pady=20)
-        self.continuebutton = Button(self.master, text="Continue", command=None, font=("Roboto" , 24) , background="#fee1c7" , foreground="#110b11")
+        self.continuebutton = Button(self.master, text="Continue", command=lambda : self.changeframe(self.nextframe), font=("Roboto" , 24) , background="#fee1c7" , foreground="#110b11")
         self.continuebutton.place(x=1000,y=650)
         self.explanationtext.place(x=100,y=200)
         self.explanationheader.place(x=100,y=50,anchor="w")
 
-class Playingboard :
-    def __init__(self,master,player1position,player2position,popuptype,playernumber,playerscore1,playerscore2) :
-        self.master = Frame(master, width=1280 , height=720,background="#52d1dc")
-        self.playernumber = playernumber#This is the player turn indicator
+class Playingboard(SNLFrame) :
+    def __init__(self,master,player1position,player2position,playerturn,playerscore1,playerscore2) :
+        super().__init__(master)
+        self.playernumber = playerturn#This is the player turn indicator
         self.player1position,self.player2position = player1position,player2position
-        self.variable = 0
+        self.variable = 0        
+
         #canvas grid system
         self.canvas = Canvas(self.master , width=600 , height= 600 , background="#53599a")
         def create_grid(event=None):
@@ -254,6 +228,9 @@ class Playingboard :
         self.canvas.bind('<Configure>', create_grid)
         #player demarcation below
         self.canvas.create_rectangle(0,600,30,570,fill="red")
+
+
+
         #label declarations
         self.labeltitle = Label(self.master , text= " THE SHOWDOWN BOARD " , font=("Corbel" , 36), foreground="#110b11" , padx = 10 ,pady = 10 )
         self.player1label = Label(self.master , text="Player 1 ",font=("Corbel" ,24),background="#52d1dc")
@@ -265,14 +242,13 @@ class Playingboard :
         self.player1counter = Label(self.master, text="0" , font=("Corbel" ,24),background="#52d1dc")
         self.player2counter = Label(self.master, text="0" , font=("Corbel" ,24),background="#52d1dc")
         self.close_button = Button(self.master, text="Quit Game", command=master.quit , font=("Roboto" , 24) , background="#fee1c7" , foreground="#110b11")
-        self.rolldicebutton = Button(self.master, text="Roll DIce", command=lambda :self.rollinganimation(2) , font=("Roboto" , 24) , background="#fee1c7" , foreground="#110b11")
+        self.rolldicebutton = Button(self.master, text="Roll Dice", command=lambda :self.rollinganimation(playerturn) , font=("Roboto" , 24) , background="#fee1c7" , foreground="#110b11")
         #Popup system with waiting here
         self.popupsnake = Label(self.master)
         self.popupladder = Label(self.master)
         self.popupnextplayer = Label(self.master)
         self.popuprandomhit = Label(self.master)
-        self.popuptype = popuptype
-        if self.popuptype == 'nextplayer':
+        """if self.popuptype == 'nextplayer':
             self.popupnextplayer.place(x=300,y=300)
         elif self.popuptype == "snake" :
             self.popupsnake.place(x=300,y=300)
@@ -281,7 +257,7 @@ class Playingboard :
         elif self.popuptype == 'gotquestion' :
             self.popuprandomhit.place(x=300,y=300)
         else:
-            pass
+            pass"""
 
         
         #Below Are positional placements
@@ -298,18 +274,21 @@ class Playingboard :
         self.player2counter.place(x=1000,y=350)
         self.rolldicebutton.place(x=100,y=600)
 
-    def rollinganimation(self,outcomenumber,playernumber):
-        
+
+    def rollinganimation(self,playernumber):
         """self.variable  = random.randint(1,6)
         self.dice1 = Label(self.master , text= self.variable , background="#fee1c7" , font = ("Roboto" , 36 ),height=2 , width= 4 )
         self.dice1.place(x=100,y=200)"""
         time.sleep(0.5)
-        if playernumber == 1:
+        outcomenumber = random.randint(1,6)
+        #Reminder that true is player1, player2
+        if playernumber == True:
             self.dice1 = Label(self.master , text= outcomenumber , background="#fee1c7" , font = ("Roboto" , 36 ),height=2 , width= 4 )
             self.dice1.place(x=100,y=200)
-        elif playernumber == 2:
+        elif playernumber == False:
             self.dice2 = Label(self.master , text= outcomenumber , background="#fee1c7",height=2 , width= 4 ,font = ("Roboto" , 36 ))
             self.dice2.place(x=100, y=400)
+        self.changeframe(Answerframe(self.rootmaster))
 
 
 
@@ -318,15 +297,7 @@ class Playingboard :
 
 
 
-class GUILOOP() :
-    def __init__(self) :
-        self.master = Tk()
-        self.master.geometry("1280x720")
-        self.master.configure(background="#52d1dc")
-        #self.nextframe = Playingboard(self.master,10,1,'ladder',1,None,None)
-        self.nextframe = Explanationframe(self.master)
-        self.start  = Introframe(self.master,self.nextframe)
-        self.master.mainloop()
+
 a = GUILOOP()
 
 
