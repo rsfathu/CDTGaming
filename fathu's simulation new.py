06b4e945498ad058cf153b95b1494a06074e30f7
rsfathu <94110228+rsfathu@users.noreply.github.com>
2021-12-02 18:31:29 +0800

    Update fathu's simulation new.py

--- fathu's simulation new.py
+++ fathu's simulation new.py
@@ -18,35 +18,32 @@ class p1_turn():
 
     def snake(self,i1):
         # messagebox.showerror("Snake","You stepped on snake! Answer this qn correctly or else you will have to fall back to tile {}".format(i1))
-        print("Snake",
-              "You stepped on snake! Answer this qn correctly or else you will have to fall back to tile {}".format(i1))
+        print("Snake","You stepped on snake! Answer this qn correctly or else you will have to fall back to tile {}".format(i1))
 
     def ladder(self,i1):
-        # messagebox.showerror("Ladder","You stepped on ladder! Answer this qn correctly or else you will have to fall back to tile {}".format(i1))
+        # messagebox.showerror("Ladder","You stepped on ladder! Answer this qn correctly to move to tile {}".format(i1))
         print("Ladder", "You stepped on ladder! Answer this qn correctly to move to tile {}".format(i1))
 
     def surprise(self,i1):
         # messagebox.showerror("Surprise","You stepped on surprise! Answer this qn correctly or else you will have to fall back to tile {}".format(i1))
-        print("Surprise",
-              "You stepped on surprise! Answer this qn correctly or else you will have to fall back to tile {}".format(
-                  i1))
+        print("Surprise","You stepped on surprise! Answer this qn correctly or else you will have to fall back to tile {}".format(i1))
         
-while end == 0:
-    p1.roll_dice()
+def check_prompt(p1.index):
     for i in range(len(snake_ls)):
         if p1.index == snake_ls[i][0]:
-            b = p1.snake(snake_ls[i][1])
+            i1 = snake_ls[i][1]
+            b = p1.snake(i1)
         else:
             pass
     for i in range(len(ladder_ls)):
         if p1.index == ladder_ls[i][0]:
-            b = p1.ladder(ladder_ls[i][1])
+            i1 = ladder_ls[i][1]
+            b = p1.ladder(i1)
         else:
             pass
     for i in range(len(surprise_ls)):
         if p1.index == surprise_ls[i][0]:
-            b = p1.surprise(surprise_ls[i][1])
+            i1 = surprise_ls[i][1]
+            b = p1.surprise(i1)
         else:
             pass
-    print(p1.bot_left())
-    print("p1 turn done")
